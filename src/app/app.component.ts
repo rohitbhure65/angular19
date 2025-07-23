d80197e947f222987b6ad8a98e5c38c5c2690aeb
Rohit Bhure <rohitbhure.cse@gmail.com>
2025-07-23 16:56:53 +0530

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular19';
+  title = 'HELLO ANGULAR19';
 }
